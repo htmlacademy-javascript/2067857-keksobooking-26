f595239a3a9f510ef3c0f6e869839f7ce95505ff
YaninaGazizovna <104156386+YaninaGazizovna@users.noreply.github.com>
2022-06-06 17:15:54 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function getRandomIntInclusive(min, max) {
   if(min < 0 || max <= min) {
     return;
   }
+
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 getRandomIntInclusive(5,4);
@@ -14,6 +15,7 @@ function getRandomArbitrary(min, max) {
   if(min < 0 || max <= min){
     return;
   }
+
   return Math.random() * (max - min) + min;
 }
 getRandomArbitrary(3,6);
