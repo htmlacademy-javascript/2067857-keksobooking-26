346553efbe0d398325537fda3bb3565078ddbe76
YaninaGazizovna <yanina.mavlyutov@mail.ru>
2022-06-24 14:45:06 +0300

    Mодуляция

---
+++ js/modules/additional-functions.js
@@ -0,0 +1,17 @@
+import {SIMILAR_PROPOSAL_COUNT} from './data';
+
+function getAvatarArray(image) {
+  const createAvatar = [];
+
+  for (let i = 0; i <= SIMILAR_PROPOSAL_COUNT; i++) {
+    if (i < 10) {
+      createAvatar[i] = `${image}0${i}.png`;
+    } else {
+      createAvatar[i] = `${image + i}.png`;
+    }
+  }
+  //возвращает массив с индекса 1,потому как нет картинки под номером '00'
+  return createAvatar.slice(1, createAvatar.length);
+}
+
+export {getAvatarArray};
---
+++ js/modules/main.js
@@ -0,0 +1,3 @@
+import './data.js';
+import {createProposals} from './data.js';
+
