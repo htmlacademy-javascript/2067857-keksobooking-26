32189c1e141b551c2a464d2a7a5f33f96637a1e4
YaninaGazizovna <104156386+YaninaGazizovna@users.noreply.github.com>
2022-07-15 16:42:02 +0600

    Правда или действие (часть 1) (#8)
    * 1
    
    * Модуляция (#5)
    
    * 1
    
    * Mодуляция
    
    * Поправила модульность
    
    * удалена папка модуль, файлы в корне js
    
    * Отрисуй меня полностью (часть 1) (#6)
    
    * 1
    
    * Модуляция (#5)
    
    * 1
    
    * Mодуляция
    
    * Поправила модульность
    
    * удалена папка модуль, файлы в корне js
    
    * popup modul added
    
    * updated
    
    * upd
    
    * разделение на функции
    
    * вынесла функцию в модуль
    
    * и прошлась prettier
    
    * добавила проверку
    
    * исправления
    
    * устранение замечаний
    
    * changes
    
    * changed
    
    * changed
    
    * last changes
    
    * new one changes
    
    * updated
    
    * Отрисуй меня полностью (часть 2) (#7)
    
    * 1
    
    * Модуляция (#5)
    
    * 1
    
    * Mодуляция
    
    * Поправила модульность
    
    * удалена папка модуль, файлы в корне js
    
    * Отрисуй меня полностью (часть 1) (#6)
    
    * 1
    
    * Модуляция (#5)
    
    * 1
    
    * Mодуляция
    
    * Поправила модульность
    
    * удалена папка модуль, файлы в корне js
    
    * popup modul added
    
    * updated
    
    * upd
    
    * разделение на функции
    
    * вынесла функцию в модуль
    
    * и прошлась prettier
    
    * добавила проверку
    
    * исправления
    
    * устранение замечаний
    
    * changes
    
    * changed
    
    * changed
    
    * last changes
    
    * new one changes
    
    * updated
    
    * Отрисуй меня полностью (часть 2)
    
    * done
    
    * 1
    
    * commit
    
    * all
    
    * first submition
    
    * resolved
    
    * conflicts res-d
    
    * изменила наименование константы
    
    * final
    
    * changes
    
    * changes
    
    * changes
    
    * last version

---
+++ js/form-validation.js
@@ -0,0 +1,103 @@
+import {
+  minPriceAmount,
+  ROOM_WORDS,
+  GUEST_WORDS,
+  capacityGuestsOptions,
+  MIN_TITLE_STRING_LENGTH,
+  MAX_TITLE_STRING_LENGTH,
+} from './data.js';
+import { getPluralWord } from './util.js';
+
+const proposalForm = document.querySelector('.ad-form');
+const titleField = proposalForm.querySelector('#title');
+const priceField = proposalForm.querySelector('#price');
+const houseTypeElements = proposalForm.querySelectorAll('[name="type"]');
+const quantityRoomsElements = proposalForm.querySelectorAll('[name="rooms"]');
+const capacityGuestsElements = proposalForm.querySelectorAll('[name="capacity"]');
+
+const pristine = new Pristine(proposalForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'p',
+  errorTextClass: 'ad-form__error',
+});
+
+function validateTitle(value) {
+  return value.length >= MIN_TITLE_STRING_LENGTH && value.length <= MAX_TITLE_STRING_LENGTH;
+}
+
+function validatePrice(value) {
+  return value.length >= 0 && parseInt(value, 10) >= minPriceAmount[houseTypeElements[0].value];
+}
+
+function getTypeErrorMessage() {
+  return `${'от'} ${MIN_TITLE_STRING_LENGTH}
+ ${'до'} ${MAX_TITLE_STRING_LENGTH} ${'символов'}`;
+}
+
+function onTypeChange() {
+  priceField.placeholder = minPriceAmount[this.value];
+  pristine.validate(priceField);
+}
+
+houseTypeElements.forEach((item) => {
+  item.addEventListener('change', onTypeChange);
+});
+
+function getPriceErrorMessage() {
+  return `Не менее ${minPriceAmount[houseTypeElements[0].value]} ₽ в выбранной категории`;
+}
+
+function validateCapacity() {
+  return capacityGuestsOptions[quantityRoomsElements[0].value].includes(
+    capacityGuestsElements[0].value
+  );
+}
+
+function onCapacityChange() {
+  quantityRoomsElements[0].placeholder = capacityGuestsOptions[this.value];
+  pristine.validate(quantityRoomsElements[0]);
+
+  capacityGuestsElements[0].placeholder = capacityGuestsOptions[this.value];
+  pristine.validate(capacityGuestsElements);
+}
+
+capacityGuestsElements.forEach((item) => {
+  item.addEventListener('change', onCapacityChange);
+});
+
+quantityRoomsElements.forEach((item) => {
+  item.addEventListener('change', onCapacityChange);
+});
+
+function getCapacityErrorMessage() {
+  return `${quantityRoomsElements[0].value}  ${getPluralWord(
+    quantityRoomsElements[0].value,
+    ROOM_WORDS
+  )}
+  ${'для '}${capacityGuestsElements[0].value} ${getPluralWord(
+  capacityGuestsElements[0].value,
+  GUEST_WORDS
+)}
+  ${':неприменимо'}`;
+}
+
+function addFormSubmitHandler() {
+  proposalForm.addEventListener('submit', (evt) => {
+    const isValid = pristine.validate();
+    if (!isValid) {
+      evt.preventDefault();
+    }
+
+    pristine.validate();
+  });
+}
+
+addFormSubmitHandler();
+
+pristine.addValidator(titleField, validateTitle, getTypeErrorMessage);
+pristine.addValidator(priceField, validatePrice, getPriceErrorMessage);
+pristine.addValidator(quantityRoomsElements[0], validateCapacity, getCapacityErrorMessage);
+pristine.addValidator(capacityGuestsElements[0], validateCapacity, getCapacityErrorMessage);
