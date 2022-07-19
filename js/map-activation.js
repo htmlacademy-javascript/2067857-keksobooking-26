42a9bfbe35ff70309643c7ec15d97374abaa601a
YaninaGazizovna <yanina.mavlyutov@mail.ru>
2022-07-19 12:23:12 +0600

    commit

---
+++ js/map-activation.js
@@ -0,0 +1,69 @@
+import { proposalCards, createProposalPopup } from './popup.js';
+import { MainPinCoordinate } from './data.js';
+
+const map = L.map('map-canvas');
+
+const mapInst = () => {
+  map.setView(
+    {
+      lat: MainPinCoordinate.LAT,
+      lng: MainPinCoordinate.LNG,
+    },
+    10
+  );
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+};
+
+mapInst();
+
+const onMapLoad = (cb) => map.on('load', cb);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const icon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: MainPinCoordinate.LAT,
+    lng: MainPinCoordinate.LNG,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  }
+);
+
+mainPinMarker.addTo(map);
+
+const markerGroup = L.layerGroup().addTo(map);
+
+const createMarkers = (proposal) => {
+  const marker = L.marker(
+    {
+      lat: proposal.location.lat,
+      lng: proposal.location.lng,
+    },
+    {
+      icon,
+    }
+  );
+  marker.addTo(markerGroup).bindPopup(createProposalPopup(proposal));
+};
+
+proposalCards.forEach((proposal) => {
+  createMarkers(proposal);
+});
+
+export { mainPinMarker, onMapLoad, mapInst };
