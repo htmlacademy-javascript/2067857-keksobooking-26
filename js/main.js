81c3f91205fa7829dc035992ea6447289a496688
YaninaGazizovna <104156386+YaninaGazizovna@users.noreply.github.com>
2022-07-21 15:07:18 +0600

    Надо подкачаться (#11)
    * commit
    
    * module9
    
    * conf res
    
    * conf
    
    * conf
    
    * conf
    
    * Надо подкачаться
    
    * conf
    
    * conf
    
    * conf
    
    * conf
    
    * conf
    
    * conf
    
    * confl existed-res-ved
    
    * conf
    
    * conf
    
    * conf
    
    * conf
    
    * надо подкачаться
    
    * устранение замечаний
    
    * добавила цвет к классу ошибках

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-import './data.js';
-import './popup.js';
 import './actions-with-forms.js';
-import './form-validation.js';
-import './map-activation.js';
-import './slider-creation.js';
+import { addFormSubmitHandler } from './form-validation.js';
+import { closeSuccessMessage } from './submit-alert-messages.js';
+import { getData } from './api.js';
+import { SIMILAR_PROPOSAL_COUNT } from './data.js';
+import { createMarkers } from './map-activation.js';
+
+getData((proposals) => {
+  proposals.slice(0, SIMILAR_PROPOSAL_COUNT).forEach((proposal) => {
+    createMarkers(proposal);
+  });
+});
+
+addFormSubmitHandler(closeSuccessMessage);
